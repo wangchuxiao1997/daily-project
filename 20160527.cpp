3ed0bf3a208985f78d199a9f9029ea90ff8294ed
wangchuxiao1997 <skull9@qq.com>
2016-05-30 21:43:38 +0800

    Update 20160527.cpp

--- 20160527.cpp
+++ 20160527.cpp
@@ -4,10 +4,10 @@ int main(void)
   char word[10];
   int i;
   printf("input");
-  for(i=0;i<10;i++)
+  for(i=0;i<10;i++) /*建立字符数组*/
 	scanf("%c",&word[i]);
   printf("output:");
-  for(i=9;i>=0;i--)
+  for(i=9;i>=0;i--)/*逆序输出数组*/
 	printf("%c",word[i]);
   printf("\n");
   return 0;
