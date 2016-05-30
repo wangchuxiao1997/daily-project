867c0060f26216c958a49e88bb6fe5d420aa69e6
wangchuxiao1997 <skull9@qq.com>
2016-05-30 22:14:16 +0800

    Update 20160528.cpp

--- 20160528.cpp
+++ 20160528.cpp
@@ -8,7 +8,7 @@ int main(void)
 	for(i=0;i<10;i++)
 	{
 		a[i]=rand()%100;
-		printf("%4d",a[i]);
+		printf("%4d",a[i]);/*生成10个随机数输出*/
 	}
 		for(i=1;i<10;i++)
 			for(j=0;j<N-1;j++)
@@ -17,10 +17,10 @@ int main(void)
 		b=a[j];
 		a[j]=a[j+1];
 		a[j+1]=b;
-	}
+	}/*双循环排大小 交换赋值*/
 	printf("\nresult:");
 	for(i=0;i<10;i++)
-		printf("%4d",a[i]);
+		printf("%4d",a[i]);/*按排好的从大到小顺序输出*/
 	printf("\n");
 	return 0;
 }
