22e1b5f99935e8d754bcd4e21bcdf306e8b80691
wangchuxiao1997 <skull9@qq.com>
2016-05-30 18:32:48 +0800

    Update 20150527.cpp

--- 20150527.cpp
+++ 20150527.cpp
@@ -4,14 +4,14 @@ int main(void)
   int a[21];
   int i;
   a[1]=1;
-  a[2]=1;
+  a[2]=1; /*定义变量赋值*/
   for(i=3;i<21;i++)
-      a[i]=a[i-1]+a[i-2];
+      a[i]=a[i-1]+a[i-2];/*设定a[i]数组*/
   for(i=1;i<21;i++)
   {
-    printf("%10d",a[i]);
+    printf("%10d",a[i]);/*输出数组*/
     if(i%5==0)
-       printf("\n");
+       printf("\n");/*如果a[i]被5整除 换行*/
   }
    return 0;
 }
