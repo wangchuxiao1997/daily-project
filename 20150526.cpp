0f53cb003363cfd9b164f5e13f99987bacad920e
wangchuxiao1997 <skull9@qq.com>
2016-05-30 18:20:20 +0800

    Update 20150526.cpp

--- 20150526.cpp
+++ 20150526.cpp
@@ -2,17 +2,17 @@
 #define N 10
 int main(void)
 {
-  int a[N],i,j;
-  for(i=0;i<N;i++)
+  int a[N],i,j; /*定义变量*/
+  for(i=0;i<N;i++) /* for循环 输入 0-9*/
   	scanf("%d",&a[i]);     
-	for(i=0;i<N-1;i++)
-	 if(a[i]>a[i+1])
+	for(i=0;i<N-1;i++) 
+	 if(a[i]>a[i+1]) /*排序,如果a[i]>a[i=1],交换变量的值*/
  	{ 
 	 a[N+1]=j;
 	 a[N+1]=a[N];
 	 a[N]=j;
 	}
-	for(i=0;i<N;i++)
+	for(i=0;i<N;i++)/*输出*/
 	  printf("%d",a[i]);
 	return 0;
 }         
