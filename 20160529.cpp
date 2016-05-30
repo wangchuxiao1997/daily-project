78c87c0d96bc1cfae7c359447fcfafe4f546a2c2
wangchuxiao1997 <skull9@qq.com>
2016-05-30 22:22:14 +0800

    Update 20160529.cpp

--- 20160529.cpp
+++ 20160529.cpp
@@ -3,11 +3,11 @@
 int main(void)
 {	
 	int a[N];
-	int * p=a,i;
+	int * p=a,i;/*定义指针变量*/
 	for(i=0;i<N;i++)
-		scanf("%d",p++);
-	p=a;
+		scanf("%d",p++);/*输入数组*/
+	p=a;/*指针p指向数组a*/
 	for(i=0;i<N;i++)
-		printf("%2d",*p++);
+		printf("%2d",*p++);/*利用指针输出数组*/
 	return 0;
 }
