69ed229a57629aae1622d093825af6a6cac5b219
wangchuxiao1997 <skull9@qq.com>
2016-06-10 22:25:19 +0800

    Create 20160610.cpp

---
+++ 20160610.cpp
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main(void)
+{ 
+  int x,y,z,t; 
+  scanf("%d%d%d",&x,&y,&z); 
+  if (x>y) /*交换x,y的值*/ 
+  if(x>z)/*交换x,z的值*/ 
+  if(y>z) /*交换z,y的值*/
+  printf("small to big: %d %d %d\n",x,y,z); 
+  return 0;
+} 
