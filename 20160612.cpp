dc35c2e9c19ab9eff426d31c0180f1dd2c256edc
wangchuxiao1997 <skull9@qq.com>
2016-06-12 23:02:49 +0800

    Create 20160612.cpp

---
+++ 20160612.cpp
@@ -0,0 +1,27 @@
+# include <stdio.h>
+# include <conio.h>
+int main(void)
+{
+  long int i;
+  int bonus1,bonus2,bonus4,bonus6,bonus10,bonus;
+  scanf("%ld",&i); 
+  bonus1=100000*0. 1;
+  bonus2=bonus1+100000*0.75;
+  bonus4=bonus2+200000*0.5;
+  bonus6=bonus4+200000*0.3;
+  bonus10=bonus6+400000*0.15;
+  if(i<=100000)
+    bonus=i*0.1;
+    else if(i<=200000)
+      bonus=bonus1+(i-100000)*0.075;
+        else if(i<=400000)
+          bonus=bonus2+(i-200000)*0.05;
+            else if(i<=600000)
+              bonus=bonus4+(i-400000)*0.03;
+                else if(i<=1000000)
+                  bonus=bonus6+(i-600000)*0.015;
+                    else
+                      bonus=bonus10+(i-1000000)*0.01;/*在同一个 if 结构中可以有多个 elseif 语句*/
+  printf("bonus=%d",bonus);
+  return 0; 
+}
